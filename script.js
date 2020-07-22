0b6c9e777a69119e26f50df4df6728270658d8fe
Valerie De Leon <valerie.deleon.school@gmail.com>
2020-07-22 21:32:55 +0000

    Mission 1

---
+++ script.js
@@ -0,0 +1,8 @@
+var first = "Karlie";
+var last = "Kloss";
+
+console.log(`${first}${last}`);
+console.log(`${last}${first}`);
+console.log(`${first} ${last}`);
+console.log(`${last} ${first} ${last} ${first}`);
+console.log(`I love ${first}`);
